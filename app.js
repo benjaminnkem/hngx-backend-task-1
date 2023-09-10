03fde5f849e30120561a2a166f0c4885e28e5037
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-09-10 13:35:40 +0100

    added default response for /

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (_, res) => {
+  res.status(200).json({
+    info: "ENDPoint available at /api"
+  })
+})
+
 app.get("/api", (req, res) => {
   const { track, slack_name } = req.query;
 
   const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const currentDay = weekdays[new Date().getDay()];
   const utc_time = new Date();
-  const github_file_url = "https://github.com/username/repo/blob/main/file_name.ext";
+  const github_file_url = "https://github.com/benjaminnkem/hngx-backend-task-1/blob/master/app.js";
   const github_repo_url = "https://github.com/benjaminnkem/hngx-backend-task-1";
 
-  return res.status(200).json({
+  res.status(200).json({
     slack_name: slack_name ? slack_name : "",
     track: track ? track : "",
     currentDay,
