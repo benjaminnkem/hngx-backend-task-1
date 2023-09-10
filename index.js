b5a46805701b78193e53358a015c26bb8165dddc
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-09-10 14:15:14 +0100

    renamed current_day

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ app.get("/api", (req, res) => {
   const { track, slack_name } = req.query;
 
   const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const currentDay = weekdays[new Date().getDay()];
-  const utc_time = new Date();
+  const current_day = weekdays[new Date().getDay()];
+  const utc_time = new Date().toISOString();
   const github_file_url = "https://github.com/benjaminnkem/hngx-backend-task-1/blob/master/index.js";
   const github_repo_url = "https://github.com/benjaminnkem/hngx-backend-task-1";
 
   res.status(200).json({
     slack_name: slack_name ? slack_name : "",
     track: track ? track : "",
-    currentDay,
+    current_day,
     utc_time,
     github_file_url,
     github_repo_url,
     status_code: 200,
   });
 });
+
+app.listen(PORT);
 
 module.exports = app;
