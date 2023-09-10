67ec65563bff4c38d6a1fdaee0f9eac9472da984
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-09-10 13:26:59 +0100

    inital commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/api", (req, res) => {
+  const { track, slack_name } = req.query;
+
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const currentDay = weekdays[new Date().getDay()];
+  const utc_time = new Date();
+  const github_file_url = "https://github.com/username/repo/blob/main/file_name.ext";
+  const github_repo_url = "https://github.com/benjaminnkem/hngx-backend-task-1";
+
+  return res.status(200).json({
+    slack_name: slack_name ? slack_name : "",
+    track: track ? track : "",
+    currentDay,
+    utc_time,
+    github_file_url,
+    github_repo_url,
+    status_code: 200,
+  });
+});
+
+app.listen(PORT);
